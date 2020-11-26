54421a2080e8e711235a3371efb699565ec44788
Alexandre TISSOT <alexandre.tissot@epitech.eu>
2020-11-26 13:07:02 +0100

    adding the function who allign the text to the left

--- Test_Printer_597/src/main.cpp
+++ Test_Printer_597/src/main.cpp
@@ -6,6 +6,7 @@
 #define RX_PIN D3
 #define DEBUG_MODE false
 #define NB_CHAR_MAX_PRINTED 32
+#define ALLIGN_TO_LEFT true
 
 SoftwareSerial mySerial(RX_PIN, TX_PIN);
 Adafruit_Thermal printer(&mySerial);
@@ -30,10 +31,35 @@ void setup() {
   serial_input = "";
 }
 
+bool next_space(String text, int counter, int current_line) {
+  for ( ; text.charAt(counter) != ' ' || counter <= text.length(); counter++, current_line++) {
+    if (current_line > 32)
+      return(false);
+  }
+  return (true);
+}
+
+void allign_left(String text) {
+  for (int counter = 0, current_line = 0; counter <= text.length(); counter++, current_line++) {
+    if (text.charAt(counter) == ' ') {
+      if (!next_space(text, counter, current_line)) {
+        counter++;
+        printer.print('\n');
+        current_line = 0;
+      }
+    }
+    printer.print(text.charAt(counter));
+  }
+}
+
 void loop() {
   serial_input = Serial.readString();
   if (serial_input != "") {
-    printer.println(serial_input);
+    if (ALLIGN_TO_LEFT) {
+      allign_left(serial_input);
+    } else {
+      printer.println(serial_input);
+    }
     if (DEBUG_MODE) {
       Serial.println(serial_input);
     }
