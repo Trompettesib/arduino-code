d37833c3450c93bea2765361e31a3d6de4d77287
Alexandre TISSOT <alexandre.tissot@epitech.eu>
2020-11-27 19:05:52 +0100

    adding formating finished

--- Test_Printer_597/src/main.cpp
+++ Test_Printer_597/src/main.cpp
@@ -4,7 +4,7 @@
 
 #define TX_PIN D4
 #define RX_PIN D3
-#define DEBUG_MODE false
+#define DEBUG_MODE true
 #define NB_CHAR_MAX_PRINTED 32
 #define ALLIGN_TO_LEFT true
 
@@ -17,7 +17,7 @@ void setup() {
   Serial.begin(9600);
   mySerial.begin(19200);
   Serial.flush();
-  delay(5000);
+  delay(1000);
   mySerial.flush();
 
   if (DEBUG_MODE) {
@@ -29,39 +29,76 @@ void setup() {
   printer.flush();
   delay(1000);
   serial_input = "";
+  Serial.println("Ready !");
 }
 
-bool next_space(String text, int counter, int current_line) {
-  for ( ; text.charAt(counter) != ' ' || counter <= text.length(); counter++, current_line++) {
-    if (current_line > 32)
-      return(false);
+void print(char c) {
+  if (DEBUG_MODE) {
+    Serial.print(c);
+  } else {
+    printer.print(c);
   }
+}
+
+bool space_after_space(int i, int j, char *str) {
+  i++;
+  j++;
+  //Serial.println(strlen(str));
+  if (j >= NB_CHAR_MAX_PRINTED) {
+    //Serial.println("false1");
+    return (false);
+  }
+  //Serial.print("\nj = ");
+  //Serial.println(j);
+  for (; str[i] != '\0' && str[i] != ' '; i++, j++) {
+    //Serial.print("\ni = ");
+    //Serial.println(i);
+    //Serial.print("\nj = ");
+    //Serial.println(j);
+    //Serial.print("\nstr[i] = ");
+    //Serial.println(str[i]);
+    //Serial.print("\nstrlen(str) = ");
+    //Serial.println(strlen(str));
+    if (j >= NB_CHAR_MAX_PRINTED) {
+      //Serial.println("false2");
+      return (false);
+    }
+  }
+  //Serial.println("true");
   return (true);
 }
 
 void allign_left(String text) {
-  for (int counter = 0, current_line = 0; counter <= text.length(); counter++, current_line++) {
-    if (text.charAt(counter) == ' ') {
-      if (!next_space(text, counter, current_line)) {
-        counter++;
-        printer.print('\n');
-        current_line = 0;
+  //Serial.print("\n text.length = ");
+  //Serial.println(text.length());
+  char *str = new char [text.length() + 1];
+  strcpy(str, text.c_str());
+  for (int i = 0, j = 0; str[i] != '\0'; i++, j++) {
+    if (str[i] == ' ') {
+      if (!space_after_space(i, j, str)) {
+        print('\n');
+        j = 0;
+        i++;
       }
     }
-    printer.print(text.charAt(counter));
+    print(str[i]);
   }
+  delete [] str;
 }
 
 void loop() {
   serial_input = Serial.readString();
   if (serial_input != "") {
     if (ALLIGN_TO_LEFT) {
+      Serial.println(' ');
+      Serial.print("Result : ");
       allign_left(serial_input);
     } else {
-      printer.println(serial_input);
-    }
-    if (DEBUG_MODE) {
-      Serial.println(serial_input);
+      if (DEBUG_MODE) {
+        Serial.println(serial_input);
+      } else {
+        printer.println(serial_input);
+      }
     }
     serial_input = "";
   }
